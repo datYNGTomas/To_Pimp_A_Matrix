9a7257aa450dbcafa3021d2b1a7816e83370bafb
Tomas Yanez <tyanez@lclark.edu>
2016-02-26 22:25:35 -0800

    Initial commit

---
+++ app/src/main/java/com/mobiledev/topimpamatrix/MatrixMainActivity.java
@@ -0,0 +1,13 @@
+package com.mobiledev.topimpamatrix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MatrixMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matrix_main);
+    }
+}
