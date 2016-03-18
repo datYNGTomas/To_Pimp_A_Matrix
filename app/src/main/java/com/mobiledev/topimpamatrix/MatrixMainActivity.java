74e2c26e27e16e2a30140308a498704021cd6fdf
Tomas Yanez <tyanez@lclark.edu>
2016-03-17 19:47:12 -0700

    got the camera to work

--- app/src/main/java/com/mobiledev/topimpamatrix/MatrixMainActivity.java
+++ app/src/main/java/com/mobiledev/topimpamatrix/MatrixMainActivity.java
@@ -1,37 +1,52 @@
 package com.mobiledev.topimpamatrix;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MatrixMainActivity extends AppCompatActivity {
 
-    private static final int CAMERA_PIC_REQUEST_CODE = 69;
-    public static final int MEDIA_TYPE_IMAGE = 1;
+    private final String apiKey = "4571bd58-9662-4d0d-9dfa-23f0479db860";
 
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private Uri fileUri;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int SELECT_PICTURE = 1;
+    private static final int TAKE_PICTURE = 2;
+    private String mImageFullPathAndName = "";
+
+    @Bind(R.id.activity_main_camera_button)
+    Button mCameraButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix_main);
 
-        // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 
-        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+        ButterKnife.bind(this);
 
-        // start the image capture Intent
-        startActivityForResult(intent, CAMERA_PIC_REQUEST_CODE);
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
     }
 
     /**
@@ -49,20 +64,20 @@ public class MatrixMainActivity extends AppCompatActivity {
         // using Environment.getExternalStorageState() before doing this.
 
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "To Pimp A Matrix");
+                Environment.DIRECTORY_PICTURES), "ToPimpAMatrix");
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.d("To Pimp A Matrix", "failed to create directory");
+                Log.d("ToPimpAMatrix", "failed to create directory");
                 return null;
             }
         }
 
         // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
         File mediaFile;
         if (type == MEDIA_TYPE_IMAGE) {
             mediaFile = new File(mediaStorageDir.getPath() + File.separator +
@@ -72,5 +87,35 @@ public class MatrixMainActivity extends AppCompatActivity {
         }
 
         return mediaFile;
+    }
+
+    public void DoShowImagePicker(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_PICTURE) {
+            if (resultCode == RESULT_OK && null != data) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                assert cursor != null;
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                // get the selected image full path and name
+                mImageFullPathAndName = cursor.getString(columnIndex);
+                cursor.close();
+            }
+        }
+    }
+
+    public void DoTakePhoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, TAKE_PICTURE);
     }
 }
